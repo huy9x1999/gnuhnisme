7093dd70400e5ba5d6b0eeed766203a80339b006
huydv <dohuy18121999@gmail.com>
2025-07-27 22:46:06 +0700

    com

--- app/page/HomePage.tsx
+++ app/page/HomePage.tsx
@@ -32,13 +32,13 @@ export default function HomePage() {
           alt="bg"
           className="w-full h-full object-cover block absolute left-0 top-0 bottom-0 right-0"
         />
-         <header className="fixed left-[66px] top-[52px]">
-          <Image src={logo} alt="logo" />
+         <header className="2xl:w-auto w-50 fixed left-[66px] 2xl:top-[52px] top-[32px]">
+          <Image src={logo} alt="logo" className="w-full" />
         </header>
         {/* content */}
         <div>
           {/* anh */}
-          <div className="z-10 2xl:w-[65%] sm:w-[55%] w-[55%] absolute -left-[7%] -bottom-[6%]">
+          <div className="z-10 2xl:w-[65%] w-[52%] absolute -left-[7%] -bottom-[6%]">
             <div className="group">
             <Image  src={CharBlue} alt="char-blue" />
             <Image src={Popup} alt="popup" className="opacity-0 group-hover:opacity-100 transition-opacity duration-500 2xl:w-[45%] w-[40%] absolute right-[10%] -top-[25%]" />
@@ -46,7 +46,7 @@ export default function HomePage() {
             <ClockSwing />
           </div>
           {/* thong tin */}
-          <div className="z-20 w-[50%] absolute right-12 top-28 font-">
+          <div className="z-20 2xl:w-[50%] w-[43%] absolute right-12 2xl:top-28 top-20">
             <div className="">
             <Image className="" src={Text1} alt="" />
             </div>
@@ -56,10 +56,10 @@ export default function HomePage() {
           </div>
         </div>
         {/* //menu */}
-        <div className="font-bold text-2xl text-[#007DC5] px-9 gap-10 fixed bottom-6 right-6 rounded-[12px] bg-[#FEEE52] min-h-16 flex items-center justify-between">
-          <span className="cursor-pointer"><Image src={TextWorks} alt="" className=""/></span>
-          <span className="cursor-pointer"><Image src={TextHome} alt="" className=""/></span>
-          <span className="cursor-pointer"><Image src={TextContent} alt="" className=""/></span>
+        <div className="font-bold text-2xl text-[#007DC5] 2xl:px-9 px-6 2xl:gap-10 gap-6 fixed bottom-6 right-6 rounded-[12px] bg-[#FEEE52] 2xl:min-h-16 min-h-12 flex items-center justify-between">
+          <span className="cursor-pointer 2xl:scale-0 scale-[0.7]"><Image src={TextWorks} alt="" className=""/></span>
+          <span className="cursor-pointer 2xl:scale-0 scale-[0.7]"><Image src={TextHome} alt="" className=""/></span>
+          <span className="cursor-pointer 2xl:scale-0 scale-[0.7]"><Image src={TextContent} alt="" className=""/></span>
         </div>
       </div>
     </>
