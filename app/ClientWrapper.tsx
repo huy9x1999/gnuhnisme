5aa7077ddab30cb969b72d8f13d82e2a0928ffff
huydv <dohuy18121999@gmail.com>
2025-08-08 12:08:07 +0700

    fix

--- app/ClientWrapper.tsx
+++ app/ClientWrapper.tsx
@@ -13,24 +13,6 @@ export default function ClientWrapper({
   const pathname = usePathname();
   const shouldHide = pathname.startsWith("/myworks");
 
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    // Đợi DOM render xong mới scroll
-    setTimeout(() => {
-      const hash = window.location.hash;
-      if (hash) {
-        const id = hash.slice(1); // bỏ dấu #
-        const el = document.getElementById(id);
-        if (el) {
-          el.scrollIntoView({ behavior: "smooth" });
-        }
-      } else {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }
-    }, 50); // delay nhỏ để DOM đảm bảo đã mount
-  }, [pathname, searchParams]);
-
   return (
     <Suspense>
       <div>
--- app/not-found/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFoundLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-      <body style={{ background: "#f8f8f8" }}>
-        {children}
-      </body>
-    </html>
-  );
-}
